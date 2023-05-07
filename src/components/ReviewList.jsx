da298e3ae43ae06d50aa9cbbbaf9415a1f5ed144
junvely <junvely97@gmail.com>
2023-05-07 20:17:29 +0900

    Feat : WineListPage 디자인 수정 및 DetailPage 디자인

---
+++ src/components/ReviewList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "../styles/css/detailPage.module.css";
+
+function ReviewList() {
+  return <div className={styled.listCon}>d</div>;
+}
+
+export default ReviewList;
